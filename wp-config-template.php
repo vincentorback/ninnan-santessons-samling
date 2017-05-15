3434a8a81726ebba903a0f98393d60a42997b071
vincentorback <vorback@gmail.com>
2017-05-16 01:02:21 +0200

    add wp-config-template

---
+++ wp-config-template.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Baskonfiguration för WordPress.
+ *
+ * Denna fil används av wp-config.php-genereringsskript under installationen.
+ * Du behöver inte använda webbplatsen, du kan kopiera denna fil direkt till
+ * "wp-config.php" och fylla i värdena.
+ *
+ * Denna fil innehåller följande konfigurationer:
+ *
+ * * Inställningar för MySQL
+ * * Säkerhetsnycklar
+ * * Tabellprefix för databas
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL-inställningar - MySQL-uppgifter får du från ditt webbhotell ** //
+/** Namnet på databasen du vill använda för WordPress */
+define('DB_NAME', '');
+
+/** MySQL-databasens användarnamn */
+define('DB_USER', '');
+
+/** MySQL-databasens lösenord */
+define('DB_PASSWORD', '');
+
+/** MySQL-server */
+define('DB_HOST', '');
+
+/** Teckenkodning för tabellerna i databasen. */
+define('DB_CHARSET', 'utf8');
+
+/** Kollationeringstyp för databasen. Ändra inte om du är osäker. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Unika autentiseringsnycklar och salter.
+ *
+ * Ändra dessa till unika fraser!
+ * Du kan generera nycklar med {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * Du kan när som helst ändra dessa nycklar för att göra aktiva cookies obrukbara, vilket tvingar alla användare att logga in på nytt.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
+
+/**#@-*/
+
+/**
+ * Tabellprefix för WordPress Databasen.
+ *
+ * Du kan ha flera installationer i samma databas om du ger varje installation ett unikt
+ * prefix. Endast siffror, bokstäver och understreck!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * För utvecklare: WordPress felsökningsläge.
+ *
+ * Ändra detta till true för att aktivera meddelanden under utveckling.
+ * Det är rekommderat att man som tilläggsskapare och temaskapare använder WP_DEBUG
+ * i sin utvecklingsmiljö.
+ *
+ * För information om andra konstanter som kan användas för felsökning,
+ * se dokumentationen.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', true);
+
+
+define('WP_CONTENT_DIR', realpath(dirname(__FILE__))  . '/wp-content');
+define('WP_CONTENT_URL', 'http://ninnan.dev/wp-content');
+define('WP_PLUGIN_DIR',  realpath(dirname(__FILE__)) . '/wp-content/plugins');
+define('WP_PLUGIN_URL', 'http://ninnan.dev/wp-content/plugins');
+
+/* Det var allt, sluta redigera här! Blogga på. */
+
+/** Absoluta sökväg till WordPress-katalogen. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Anger WordPress-värden och inkluderade filer. */
+require_once(ABSPATH . 'wp-settings.php');
